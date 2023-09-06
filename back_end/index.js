60641e1b0016f3754555bb37c664855ade8ddc6a
John-Giovenco <JohnGiovenco93@gmail.com>
2023-09-06 12:34:31 -0700

    starting index for backende

--- back_end/index.js
+++ back_end/index.js
@@ -6,3 +6,12 @@ const app = express();
 
 //express settings
 app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes & Controllers
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/moves", require("./controllers/moves"));
