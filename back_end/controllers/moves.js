5fd31370e986bfa727a00d80268acbcab9adcd41
John-Giovenco <JohnGiovenco93@gmail.com>
2023-09-09 10:20:09 -0700

    finishing up move controller

--- back_end/controllers/moves.js
+++ back_end/controllers/moves.js
@@ -66,13 +66,59 @@ router.delete("/moveId", async (req, res) => {
     if (!move) {
       res
         .status(404)
-        .json({ message: `Could not find that move with ID "$${moveId}"` });
+        .json({ message: `Could not find that move with ID "${moveId}"` });
     } else {
-      Object.assign(move, req.body);
-      await move.save();
+      await move.destroy();
       res.json(move);
     }
   }
 });
 
-router.delete("/");
+router.post("/:moveId/description", async (req, res) => {
+  const moveId = Number(req.params.moveId);
+
+  req.body.submission = req.body.submission ? true : false;
+
+  const move = await Move.findOne({
+    where: { moveId: moveId },
+  });
+
+  if (!move) {
+    res
+      .status(404)
+      .json({ message: `could not find that move with the id of "${moveId}"` });
+  }
+
+  const description = await Description.create({
+    ...req.body,
+    moveId: moveId,
+  });
+
+  res.send({
+    ...description.toJSON(),
+  });
+});
+
+router.delete("/:moveId/description/:descriptionId", async (req, res) => {
+  let moveId = Number(req.params.moveId);
+  let descriptionId = Number(req.params.descriptionId);
+
+  if (isNaN(moveId)) {
+    res.status(404).json({ message: `invalid id "${moveId}` });
+  } else if (isNaN(descriptionId)) {
+    res.status(404).json({ message: `invalid id "${descriptionId}"` });
+    const description = await Description.findOne({
+      where: { descriptionId: descriptionId },
+    });
+    if (!not) {
+      res.status(404).json({
+        message: `Could not find that description with ID "${descriptionId}"`,
+      });
+    } else {
+      await description.destroy();
+      res.json(description);
+    }
+  }
+});
+
+module.exports = router;
